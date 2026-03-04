8da1d31aee9dc134e6d4981dd914e6a51989b460
aliceism <alice7ism@gmail.com>
2026-03-04 12:58:21 +0200

    refactor(hero): point primary cta to join page

--- template-parts/front-page/hero.php
+++ template-parts/front-page/hero.php
@@ -6,7 +6,7 @@
         </h1>
         <p class="hero-subtitle"><?php esc_html_e('Build systems. Build momentum. Build yourself.', 'habitlab'); ?></p>
         <div class="hero-buttons">
-            <a class="btn btn-primary" href="<?php echo esc_url(wp_registration_url()); ?>"><?php esc_html_e('Start Your HabitLab', 'habitlab'); ?></a>
+            <a class="btn btn-primary" href="<?php echo esc_url(habitlab_get_page_url_by_slug('join')); ?>"><?php esc_html_e('Start Your HabitLab', 'habitlab'); ?></a>
         </div>
     </div>
 </section>
